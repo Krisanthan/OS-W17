3366ef8b1dcb8633711a8edea5ba05985fe4b494
Krisanthan <krish.balakrishnan@mail.utoronto.ca>
2017-02-01 18:50:13 -0500

    added comment

--- CSCC69/starter_code/starter_code/interceptor.c
+++ CSCC69/starter_code/starter_code/interceptor.c
@@ -252,8 +252,10 @@ void (*orig_exit_group)(int);
 void my_exit_group(int status)
 {
 	spin_lock(&pidlist_lock);
+	// remove pid from all lists
 	del_pid(current->pid);
 	spin_unlock(&pidlist_lock);
+	// calling original exit_group
 	orig_exit_group(status);
 
 }
